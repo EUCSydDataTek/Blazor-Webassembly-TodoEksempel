88d105b84961769b7633205d0cac3159a4d1696c
jako5457 <jako5457@gmail.com>
2022-11-07 00:10:48 +0100

    Added validation

--- Todo_BlazorWebassembly_Eksempel/Models/TodoItem.cs
+++ Todo_BlazorWebassembly_Eksempel/Models/TodoItem.cs
@@ -1,12 +1,18 @@
-﻿namespace Todo_BlazorWebassembly_Eksempel.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo_BlazorWebassembly_Eksempel.Models
 {
     public class TodoItem
     {
 
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(12,ErrorMessage = "Max length for the title is 12 carecters")]
         public string Title { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(500, ErrorMessage = "Max length for the description is 500 carecters")]
         public string Description { get; set; } = string.Empty;
 
         public bool IsCompleted = false;
