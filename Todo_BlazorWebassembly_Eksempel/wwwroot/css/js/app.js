f02dc21abd85689e4ce03702445e336994834dd4
jako5457 <jako5457@gmail.com>
2022-11-07 12:09:40 +0100

    ok

---
+++ Todo_BlazorWebassembly_Eksempel/wwwroot/css/js/app.js
@@ -0,0 +1,9 @@
+﻿function Vibrate(TimeMs) {
+    console.log("vibrating for " + TimeMs + "ms.");
+    window.navigator.vibrate(TimeMs)
+}
+
+function GetBrowserLanguage() {
+    console.log("Getting language from browser")
+    return window.navigator.language
+}
