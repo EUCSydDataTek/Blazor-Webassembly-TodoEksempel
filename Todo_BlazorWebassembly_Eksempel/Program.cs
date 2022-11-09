fafb3ef0fd79a596aa86026af0f8902b4316b8cc
jako5457 <jako5457@gmail.com>
2022-11-09 11:32:25 +0100

    added toasts

--- Todo_BlazorWebassembly_Eksempel/Program.cs
+++ Todo_BlazorWebassembly_Eksempel/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Todo_BlazorWebassembly_Eksempel;
 using Todo_BlazorWebassembly_Eksempel.Services;
 using Blazored.Modal;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,6 +14,7 @@ builder.Services.AddScoped<ITodoService,TodoApiService>();
 
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredModal();
+builder.Services.AddBlazoredToast();
 
 builder.Services.AddHttpClient<ITodoService, TodoApiService>(config => config.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
